53186bff455663986d601061d52ca535f6774e16
Vashu Kaushik <itzmevashukaushik@gmail.com>
2025-07-04 20:13:25 +0530

    NewTask2

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('mobile-menu');
-    const header = document.querySelector('header');
-
-    menuToggle.addEventListener('click', function () {
-        header.classList.toggle('menu-open');
-    });
-});
